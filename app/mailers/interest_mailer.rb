d6d0616d12453ca9d3b1407ac59bfe65c4190cf2
Nicholas Petru <nicholaspetru@gmail.com>
2017-09-19 16:57:09 -0700

    Implement temporary email template display logic

--- app/mailers/interest_mailer.rb
+++ app/mailers/interest_mailer.rb
@@ -5,8 +5,8 @@ class InterestMailer < ApplicationMailer
     @user = user
     @artists = Interest.where('user_id' => @user.username).pluck(:artist_slug)
     @artworks = Interest.where('user_id' => @user.username).pluck(:artwork_slug)
-    @artwork_is_empty = @artworks.uniq.length == 1 and @artworks.include? nil
-    @artist_is_empty = @artists.uniq.length == 1 and @artists.include? nil
+    @artwork_is_empty = (@artworks.uniq.length == 1 and @artworks.include? nil) or @artworks.empty?
+    @artist_is_empty = (@artists.uniq.length == 1 and @artists.include? nil) or @artists.empty?
     mail(to: @user.email, subject: 'Here are your current interests')
   end
 end
--- test/mailers/previews/interest_mailer_preview.rb
+++ test/mailers/previews/interest_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/interest_mailer
 class InterestMailerPreview < ActionMailer::Preview
   def user_interest_mailer
-    InterestMailer.user_interest_mailer(User.fourth)
+    InterestMailer.user_interest_mailer(User.third)
   end
 end
